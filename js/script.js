94a0b8e7365179b60d0781f3606ee040fa0d849b
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-10 00:36:41 +0100

    Add all project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+    console.log("The phone brand is " + this.brand + " ,color is " + this.color + " and the price is " + this.price + ".");
+}
+
+var samsungGalaxyS6 = new Phone("Samsung", 1800, "grey");
+var iPhone6s = new Phone("Apple", 2250, "silver");
+var onePlusOne = new Phone("OnePlus", 2000, "black");
+
+samsungGalaxyS6.printInfo();
+iPhone6s.printInfo();
+onePlusOne.printInfo();
+
